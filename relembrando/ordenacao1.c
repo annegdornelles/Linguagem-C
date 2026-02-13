7d21e50abc58355fc24db34ba887bef3fd26383e
Anne G. Rufino Dornelles <gabrielleannerd@gmail.com>
2026-02-12 23:06:19 -0300

    .

--- relembrando/ordenacao1.c
+++ relembrando/ordenacao1.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+//relembrando como fzr ordenacao: bubblesort(loop, compara com os vizinhos), selection sort(pega o menor e define), qsort(proprio do C)
 
 int main(){
     int n1, n2;
 }
 
-int maior(){
+int ordenacao(){
     
 }
