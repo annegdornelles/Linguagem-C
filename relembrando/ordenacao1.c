ade8012a02b659ecc54411ffea3593d47e441028
Anne G. Rufino Dornelles <gabrielleannerd@gmail.com>
2026-02-13 21:23:08 -0300

    .

--- relembrando/ordenacao1.c
+++ relembrando/ordenacao1.c
@@ -2,7 +2,19 @@
 //relembrando como fzr ordenacao: bubblesort(loop, compara com os vizinhos), selection sort(pega o menor e define), qsort(proprio do C)
 
 int main(){
-    int n1, n2;
+    int n1, n2, n3, n4;
+
+    printf("Insira um numero:\n");
+    scanf ("%d",&n1);
+
+    printf("Insira um numero:\n");
+    scanf ("%d",&n2);
+
+    printf("Insira um numero:\n");
+    scanf ("%d",&n3);
+
+    printf("Insira um numero:\n");
+    scanf ("%d",&n4);
 }
 
 int ordenacao(){
